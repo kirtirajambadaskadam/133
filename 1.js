c192a7d8779a7ff8f55d51a89c54dc5d4ecfc314
kirtirajambadaskadam <86154528+kirtirajambadaskadam@users.noreply.github.com>
2021-12-26 19:34:19 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,44 @@
+function setup(){
+    canvas= createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modalloaded);
+    document.getElementById("status").innerHTML="Status = Detecting Object";
+}
+
+img="";
+status="";
+objects=[];
+
+function preload(){
+    img= loadImage("1i.jpg");
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(i=0 ; i < objects.length ; i++){
+            document.getElementById("status").innerHTML="Status = Object Detected";
+
+            fill("#FF0000");
+            percent= floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width, objects[i].height);            
+        }
+    }
+
+}
+
+function modalloaded(){
+    console.log("Modal Loaded!");
+    status= true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error();
+    }
+    console.log(results);
+    objects=results;
+}
